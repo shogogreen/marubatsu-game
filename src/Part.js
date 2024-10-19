84060ab44ac298e105105a839dfadc55d0249813
shogo <shogo-hana@keio.jp>
2024-10-19 18:54:30 +0900

    アイコン追加 タイトル変更 ○×の色変更

--- src/Part.js
+++ src/Part.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Part = (props) => {
   if (props.state === '.') {
-    return <button disabled={props.state === '' ? false: true} onClick={() => {props.changeState(props.id)}} style={{ fontSize: '50px' }}>〇</button>;
+    return <button disabled={true} onClick={() => {props.changeState(props.id)}} style={{ fontSize: '50px', color: 'blue' }}>〇</button>;
   } else if (props.state === 'x') {
-    return <button disabled={props.state === '' ? false: true} onClick={() => {props.changeState(props.id)}} style={{ fontSize: '50px' }}>×</button>;
+    return <button disabled={true} onClick={() => {props.changeState(props.id)}} style={{ fontSize: '50px' }}>×</button>;
+  } else if (props.state === '〇') {
+    return <button disabled={true} onClick={() => {props.changeState(props.id)}} style={{ color: 'blue' }}>{props.state}</button>;
+  } else if (props.state === '×') {
+    return <button disabled={true} onClick={() => {props.changeState(props.id)}}>{props.state}</button>;
   }
-  return <button disabled={(props.state === '' && props.isVictory === null) ? false: true} onClick={() => {props.changeState(props.id)}}>{props.state}</button>;
+  return <button disabled={props.isVictory === null ? false: true} onClick={() => {props.changeState(props.id)}}>{props.state}</button>;
 };
 
 
